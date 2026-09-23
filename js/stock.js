f64b0ba9f9de91812750aaac2e42baef57113346
djkier <djjustinekier@gmail.com>
2026-09-23 18:08:30 +0800

    Initialize stock variables to be use

---
+++ js/stock.js
@@ -0,0 +1,6 @@
+const stockForm = document.getElementById("stock-form");
+const stockProduct = document.getElementById("stock-product");
+const stockType = document.getElementById("stock-type");
+const stockQuantity = document.getElementById("stock-quantity");
+const stockError = document.getElementById("stock-error");
+const stockFeedback = document.getElementById("stock-feedback");
